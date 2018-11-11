701cedc9c761154dc38d046823494b7611252bb5
Dominique Borer <dominiqueborer@yahoo.de>
2018-11-11 13:56:21 +0100

    login, sqlworking

--- ExpressApp1/routes/index.js
+++ ExpressApp1/routes/index.js
@@ -1,10 +1,17 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 //var dbtest = require('../modules/dbtest');
 var dtr = require('../modules/testmodule');
 //var rmDB = require('../modules/randomiusmemiusDB');
-const sql = require('mssql')
+const sql = require('mssql');
+var passport = require('passport');
+require('../config/passport-config');
 
 var config = {
     user: 'randomiusmemius',
@@ -16,6 +23,29 @@ var config = {
 sql.on('error', err => {
     // ... error handler
 })
+////router.use(favicon(__dirname + './public/favicon.ico'));
+//router.use(favicon('./public/favicon.ico'));
+//router.use(logger('dev'));
+//router.use(bodyParser.json());
+//router.use(bodyParser.urlencoded({ extended: false }));
+//router.use(cookieParser());
+//router.use(express.static(path.join(__dirname, 'public')));
+
+//router.use(require('morgan')('combined'));
+//router.use(require('cookie-parser')());
+//router.use(require('body-parser').urlencoded({ extended: true }));
+//router.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+
+
+//router.use(passport.initialize());
+//router.use(passport.session());
+//passport.initialize();
+//passport.session();
+
+
+
+
 
 
 /* GET home page. */
@@ -26,7 +56,8 @@ router.get('/', function (req, res) {
     (async () => {      
 
         try {
-            let pool = await sql.connect(config)
+            let pool = await new sql.ConnectionPool(config);
+            await pool.connect();
             let rDataEntries = "";
             let result1 = await pool.request()
                 //.input('input_parameter', sql.Int, value)
@@ -41,8 +72,9 @@ router.get('/', function (req, res) {
                 'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg',
                 'rDateTime': dtr.myDateTime(),
                 'allEntries': rDataEntries//rmDB.getSqlQueryResponse(config, 'select * from rDataTable')//getSqlQuery()
+                
             }
-            res.render('index', { title: 'Express', "data": data });
+            res.render('index', { title: 'Express', "data": data, "user": req.user });
 
 
         } catch (err) {
